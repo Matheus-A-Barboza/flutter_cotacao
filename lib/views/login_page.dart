2e470b13d10a733a7fb7268007cf6854fcc634bb
Matheus <progammerantunes@gmail.com>
2024-04-21 11:29:42 -0300

    fix

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,16 +1,52 @@
 import 'package:flutter/material.dart';
 
+
 class LoginPage extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/home');  // Corrigido para navegar para '/home'
-          },
-          child: Text('Entrar'),
+      appBar: AppBar(title: Text('Entrar')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/1.png',
+              height: 190.0,
+              width: 190.0,
+              fit: BoxFit.contain,
+            ),
+            SizedBox(height: 40),
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Senha',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navegar para a tela Home sem validação de login
+                Navigator.pushNamed(context, '/home');
+              },
+              child: Text('Entrar'),
+            ),
+          ],
         ),
       ),
     );
--- lib/views/quotes_page.dart
+++ lib/views/quotes_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_atividade/model/favorite_currencies.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
@@ -10,46 +11,54 @@ class QuotesPage extends StatefulWidget {
   _QuotesPageState createState() => _QuotesPageState();
 }
 
-class _QuotesPageState extends State<QuotesPage> {
+class _QuotesPageState extends State<QuotesPage>{
   Map<String, dynamic>? _quotes;
+  bool _isLoading = true;
 
   @override
-  void initState() {
+  void initState(){
     super.initState();
     fetchQuotes();
   }
 
-  Future<void> fetchQuotes() async {
-    final response = await http.get(Uri.parse('https://economia.awesomeapi.com.br/json/last'));
+  Future<void> fetchQuotes() async{
+    final response = await http.get(Uri.parse('https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL'));
 
-    if (response.statusCode == 200) {
+    if (response.statusCode == 200){
       setState(() {
         _quotes = json.decode(response.body);
+        _isLoading = false;
+      });
+    } else {
+      setState(() {
+        _isLoading = false;
       });
     }
   }
 
   @override
-  Widget build(BuildContext context) {
-    return _quotes == null
-        ? Center(child: CircularProgressIndicator())
-        : ListView.builder(
-            itemCount: _quotes!.length,
-            itemBuilder: (context, index) {
-              String currency = _quotes!.keys.elementAt(index);
-              String value = _quotes![currency]['bid'];
+  Widget build(BuildContext context){
+    return _isLoading
+    ? Center(child: CircularProgressIndicator()):
+    (_quotes == null || _quotes!.isEmpty)
+    ? Center(child: Text('Nenhuma Cotação disponivel.'))
+    : ListView.builder(
+      itemCount: _quotes!.length,
+      itemBuilder: (cotext, index){
+        String currency = _quotes!.keys.elementAt(index);
+        String value = _quotes![currency]['bid'];
 
-              return ListTile(
-                title: Text('$currency'),
-                subtitle: Text('Valor: $value'),
-                onLongPress: () {
-                  Provider.of<FavoriteCurrencies>(context, listen: false).toggleFavorite(currency, value);
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('${currency.toUpperCase()} foi adicionado aos favoritos!')),
-                  );
-                },
-              );
-            },
-          );
+        return ListTile(
+          title: Text('$currency'),
+          subtitle: Text('Valor: $value'),
+          onLongPress: (){
+            Provider.of<FavoriteCurrencies>(context, listen: false).toggleFavorite(currency, value);
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('${currency.toUpperCase()} foi adicionado aos favoritos!')),
+            );
+          },
+        );
+      },
+    );
   }
 }
